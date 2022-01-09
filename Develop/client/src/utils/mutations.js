f3707815f9b922f9a0cc2ec4ccf397a9d06ecaf1
alexzoo0 <alexisvelasquez15@gmail.com>
2022-01-09 16:01:26 -0600

    mutations and querie files

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import gql from "graphql-tag";
+export const LOGIN_USER = gql`
+mutation loginUser($email:String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user{
+            _id
+        }
+    }
+}`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!){
+    addUser(username: $username, password: $password, email: $email){
+        user{
+            -id
+            username
+            email
+            bookCount
+            saveBooks{
+                authors
+                bookId
+                image
+                link
+                description
+            }
+        }
+        token
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: String!){
+    saveBook(input: $input){
+        _id
+        username
+        email
+        bookCount
+        saveBooks{
+            # _id
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!){
+    removeBook(bookId: $bookId){
+        _id
+        username
+        email
+        bookCount
+        saveBooks{
+            bookId
+            authors
+            image
+            link 
+            title 
+            description
+        }
+    }
+}`;
+
+
